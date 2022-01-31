0ac3eaec8de43205f0af11c73cb6c9fb53a4532e
mangoshi <soggymoose@gmail.com>
2022-01-31 22:41:23 +0000

    Imported Vuetify's 'base' layout
    - Also removed Vue's HelloWorld component, won't be needing that.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>AudioHaven API Playground</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({ drawer: null }),
 }
 </script>
 
