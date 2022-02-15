d8d10a7d1f8b0759d8c431f6480d6846669b5972
mangoshi <soggymoose@gmail.com>
2022-02-15 18:39:06 +0000

    Made data iterator to display followed artists - Made using Vuetify's most advanced example. - Has sorting/filtering/pagination. - May need to rename some variables when making extra requests

--- src/pages/Spotify.vue
+++ src/pages/Spotify.vue
@@ -51,7 +51,168 @@
 		<v-container>
 			<!--	If there is a token == You're logged-in to Spotify	-->
 			<div v-if="spotifyLoggedIn">
+				<v-data-iterator
+					:items="items"
+					:items-per-page.sync="itemsPerPage"
+					:page.sync="page"
+					:search="search"
+					:sort-by="sortBy.toLowerCase()"
+					:sort-desc="sortDesc"
+					hide-default-footer
+				>
+					<template v-slot:header>
+						<v-toolbar
+							dark
+							color="purple darken-3"
+							class="mb-1"
+						>
+							<v-text-field
+								v-model="search"
+								clearable
+								flat
+								solo-inverted
+								hide-details
+								prepend-inner-icon="mdi-magnify"
+								label="Search"
+							></v-text-field>
+							<template v-if="$vuetify.breakpoint.mdAndUp">
+								<v-spacer></v-spacer>
+								<v-select
+									v-model="sortBy"
+									flat
+									solo-inverted
+									hide-details
+									:items="keys"
+									prepend-inner-icon="mdi-magnify"
+									label="Sort by"
+								></v-select>
+								<v-spacer></v-spacer>
+								<v-btn-toggle
+									v-model="sortDesc"
+									mandatory
+								>
+									<v-btn
+										large
+										depressed
+										color="purple"
+										:value="false"
+									>
+										<v-icon>mdi-arrow-up</v-icon>
+									</v-btn>
+									<v-btn
+										large
+										depressed
+										color="purple"
+										:value="true"
+									>
+										<v-icon>mdi-arrow-down</v-icon>
+									</v-btn>
+								</v-btn-toggle>
+							</template>
+						</v-toolbar>
+					</template>
 
+					<template v-slot:default="props">
+						<v-row>
+							<v-col
+								v-for="item in props.items"
+								:key="item.name"
+								cols="12"
+								sm="6"
+								md="4"
+								lg="3"
+							>
+								<v-card>
+									<v-card-title class="subheading font-weight-bold">
+										{{ item.name }}
+									</v-card-title>
+									<v-img
+										:src="item.images[0].url"
+										:aspect-ratio="1"
+									>
+									</v-img>
+
+									<v-divider></v-divider>
+
+									<v-list dense>
+										<v-list-item
+											v-for="(key, index) in filteredKeys"
+											:key="index"
+										>
+											<v-list-item-content :class="{ 'purple--text': sortBy === key }">
+												{{ key }}:
+											</v-list-item-content>
+											<v-list-item-content
+												class="align-end"
+												:class="{ 'purple--text': sortBy === key }"
+											>
+												{{ item[key.toLowerCase()] }}
+											</v-list-item-content>
+										</v-list-item>
+									</v-list>
+								</v-card>
+							</v-col>
+						</v-row>
+					</template>
+
+					<template v-slot:footer>
+						<v-row
+							class="mt-2"
+							align="center"
+							justify="center"
+						>
+							<span class="grey--text">Items per page</span>
+							<v-menu offset-y>
+								<template v-slot:activator="{ on, attrs }">
+									<v-btn
+										dark
+										text
+										color="primary"
+										class="ml-2"
+										v-bind="attrs"
+										v-on="on"
+									>
+										{{ itemsPerPage }}
+										<v-icon>mdi-chevron-down</v-icon>
+									</v-btn>
+								</template>
+								<v-list>
+									<v-list-item
+										v-for="(number, index) in itemsPerPageArray"
+										:key="index"
+										@click="updateItemsPerPage(number)"
+									>
+										<v-list-item-title>{{ number }}</v-list-item-title>
+									</v-list-item>
+								</v-list>
+							</v-menu>
+
+							<v-spacer></v-spacer>
+
+							<span class="mr-4grey--text">
+								Page {{ page }} of {{ numberOfPages }}
+							</span>
+							<v-btn
+								fab
+								dark
+								color="purple darken-3"
+								class="mr-1"
+								@click="formerPage"
+							>
+								<v-icon>mdi-chevron-left</v-icon>
+							</v-btn>
+							<v-btn
+								fab
+								dark
+								color="purple darken-3"
+								class="ml-1"
+								@click="nextPage"
+							>
+								<v-icon>mdi-chevron-right</v-icon>
+							</v-btn>
+						</v-row>
+					</template>
+				</v-data-iterator>
 			</div>
 		</v-container>
 	</v-container>
@@ -60,6 +221,7 @@
 <script>
 // import axios from '../config/audiohaven.js'
 import router from "@/router";
+import axios from "axios";
 
 require('dotenv').config();
 
@@ -116,13 +278,46 @@ export default {
 			],
 			spotifyLoggedIn: false,
 			// Data Iterator //,
+			itemsPerPageArray: [4, 8, 12],
+			search: '',
+			filter: {},
+			sortDesc: false,
+			page: 1,
+			itemsPerPage: 4,
+			sortBy: 'name',
+			keys: [
+				'Name',
+				'Popularity',
+			],
+			items: [],
 		}
+	},
+	computed: {
+		// Data Iterator Computed Methods //
+		numberOfPages () {
+			return Math.ceil(this.items.length / this.itemsPerPage)
+		},
+		filteredKeys () {
+			return this.keys.filter(key => key !== 'Name')
+		},
 	},
 	mounted(){
 		this.checkTokens()
 		this.checkSpotifyLogin()
+		this.getFollowedArtists()
 	},
 	methods: {
+		// Data Iterator Methods //
+		nextPage () {
+			if (this.page + 1 <= this.numberOfPages) this.page += 1
+		},
+		formerPage () {
+			if (this.page - 1 >= 1) this.page -= 1
+		},
+		updateItemsPerPage (number) {
+			this.itemsPerPage = number
+		},
+		// Spotify Token Methods //
 		// spotifyOAuth(){
 		// 	axios("/spotify/login")
 		// }
@@ -157,7 +352,26 @@ export default {
 			router.go(0)
 			// Set spotifyLoggedIn to true
 			this.spotifyLoggedIn = false;
-		}
+		},
+		getFollowedArtists(){
+			if(this.spotifyLoggedIn){
+				let token = localStorage.getItem('spotify_access_token')
+				let baseURL = 'https://api.spotify.com/v1'
+				axios
+					.get(`${baseURL}/me/following?type=artist`,
+						{
+							headers: {
+								"Authorization" : `Bearer ${token}`
+							}
+						})
+					.then(response => {
+						console.log("getData() response: ", response.data)
+						this.items = response.data.artists.items
+						}
+					)
+					.catch(error => console.log("getData() error caught: ", error))
+			}
+		},
 	}
 }
 </script>
