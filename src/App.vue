9ca49737cec1fbe33a937b3381e2dc0d9f63a194
mangoshi <soggymoose@gmail.com>
2022-02-18 16:32:11 +0000

    Cleaned up sidebar link styling & added placeholder theme toggle btn

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,21 @@
 	<v-app id="app">
 
 		<!--Sidebar-->
+		<!--	TODO: Manage sidebar state https://stackoverflow.com/questions/40780730/vue-js-changing-props		-->
 		<v-navigation-drawer
 			v-model="drawer"
 			app
 		>
 			<v-list>
+				<!-- Sidebar link list -->
 				<v-list-item-group>
 					<!-- Home Page -->
-					<v-list-item class="mb-10" to="/">
-						<router-link to="/"><v-icon>mdi-home</v-icon>&nbsp;Home</router-link>
+					<v-list-item class="mb-10 sideLink" to="/">
+						<router-link to="/"><v-icon size="36" color="purple">mdi-home</v-icon>&nbsp;Home</router-link>
 					</v-list-item>
 					<!-- Spotify Page -->
-					<v-list-item class="mb-10" to="Spotify">
-						<router-link to="Spotify"><v-icon>mdi-spotify</v-icon>&nbsp;Spotify</router-link>
+					<v-list-item class="mb-10 sideLink" to="Spotify">
+						<router-link to="Spotify"><v-icon size="36" color="green">mdi-spotify</v-icon>&nbsp;Spotify</router-link>
 					</v-list-item>
 				</v-list-item-group>
 			</v-list>
@@ -23,13 +25,18 @@
 
 		<!-- Navbar -->
 		<v-app-bar app>
-			<!--	TODO: Manage sidebar state https://stackoverflow.com/questions/40780730/vue-js-changing-props		-->
+			<!-- Sidebar hamburger button (only active when logged in) -->
 			<v-app-bar-nav-icon v-if="$store.state.loggedIn" @click="drawer = !drawer"></v-app-bar-nav-icon>
-			<v-toolbar-title>AudioHaven API Playground - {{ currentRouteName }}</v-toolbar-title>
+			<!-- Page title : x + {{ route's title }} -->
+			<v-toolbar-title>AudioHaven 🛠 {{ currentRouteName }}</v-toolbar-title>
+			<!-- Spacer -->
 			<v-spacer>
 
 			</v-spacer>
-			<v-btn v-if="$store.state.loggedIn" class="v-btn accent black--text" @click="logout()">Log Out</v-btn>
+			<!--	Dark/Light mode toggle button	-->
+			<v-btn icon style="margin-right: 10px" class=""><v-icon>mdi-brightness-4</v-icon></v-btn>
+			<!--	Log-out button (bg colour = accent & text colour = black)	-->
+			<v-btn v-if="$store.state.loggedIn" class="accent black--text" @click="logout()">Log Out</v-btn>
 		</v-app-bar>
 
 		<!-- Router View -->
@@ -85,6 +92,11 @@ export default {
 	color: #2c3e50;
 }
 
+.sideLink:link *{
+	font-size: 24px;
+	text-decoration: none;
+	color: black;
+}
 /* Utility Classes */
 
 .unselectable {
