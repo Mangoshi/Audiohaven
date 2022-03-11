b81d79071ef5c6bc016e08631a1abdfed838e680
mangoshi <soggymoose@gmail.com>
2022-03-11 18:35:16 +0000

    Reverted babel preset - After reading Vue docs, it seems what Vuetify wants is included in the original preset

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  // using @vue/babel-preset-app instead of @babel/preset-env,
+  // since preset-env is included in babel-preset-app
+  // https://cli.vuejs.org/config/#babel
+  presets: ['@vue/babel-preset-app'],
   env: {
     "production": {
       "plugins": ["transform-remove-console"]
