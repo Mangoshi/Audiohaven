e140b56caead821437a877a9119a804c25c438c8
mangoshi <soggymoose@gmail.com>
2022-03-11 19:33:26 +0000

    Reverted babel-config & reinstalled packages - babel-preset-app broke everything :)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  // using @vue/babel-preset-app instead of @babel/preset-env,
-  // since preset-env is included in babel-preset-app
-  // https://cli.vuejs.org/config/#babel
-  presets: ['@vue/babel-preset-app'],
+  presets: ['@vue/cli-plugin-babel/preset'],
   env: {
     "production": {
       "plugins": ["transform-remove-console"]
