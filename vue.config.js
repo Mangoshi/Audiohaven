06c10e6e5bac6bff7a991bdc72b7a692cbff97d0
mangoshi <soggymoose@gmail.com>
2022-03-11 17:44:38 +0000

    Removed dotenv-webpack - Testing to see if this is affecting the hosted version's environment variables

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
-const Dotenv = require('dotenv-webpack');
-
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/dist/': '/',
     "transpileDependencies": [
         "vuetify"
-    ],
-    configureWebpack: {
-        plugins: [
-            new Dotenv()
-        ]
-    }
+    ]
 }
