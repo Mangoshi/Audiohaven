814a9dbb69b9c901cbe1762258b6a6badbac247c
mangoshi <soggymoose@gmail.com>
2022-03-25 18:29:43 +0000

    Updated package info & publicPath in Vue config - publicPath now points to '/Audiohaven/' instead of '/Audiohaven-Sandbox/'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/Audiohaven-Sandbox/': '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/Audiohaven/': '/',
     transpileDependencies: ["vuetify"],
     pages: {
         index: {
